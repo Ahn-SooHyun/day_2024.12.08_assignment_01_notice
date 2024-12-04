14267581be6882375e68d4bb02283703b3f3f8fe
ASH <soxyxs4996@gmail.com>
2024-12-04 09:45:23 +0900

    Feat_2024.12.04_Save_2_09:45 Axios Get 방식

--- src/comp/ax/ax01.js
+++ src/comp/ax/ax01.js
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function AX1() {
+  const [area, setArea] = useState([]);
+
+  function axios_01() {
+    console.log("==============================");
+    console.log("        Axios Get 방식");
+    console.log("==============================");
+
+    axios
+      .get("http://localhost:8080/api/area/list") //방식(URL)
+      .then((res) => {
+        console.log(res);
+        if (res.data.code == "200") {
+          setArea(res.data.data);
+        }
+      }); //성공
+
+    //Promise 라는 JavaScript 기능하고 비슷.
+    //ES 6 문법
+
+    // const aa = new Promise(resolve, reject);
+  }
+
   return (
     <div>
       <h1>Axios 연습</h1>
+      <input type="button" value="get 방식" onClick={axios_01} />
     </div>
   );
 }
