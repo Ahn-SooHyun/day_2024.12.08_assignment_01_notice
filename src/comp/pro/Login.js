8efe62060fce6b2044f9027610f676d1bca1efd7
ASH <soxyxs4996@gmail.com>
2024-12-05 10:35:21 +0900

    Feat_2024.12.05_Save_5_10:35 html Login

--- src/comp/pro/Login.js
+++ src/comp/pro/Login.js
@@ -1,14 +1,25 @@
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
 
+  const idRef = useRef("");
+  const pwRef = useRef("");
+
+  const loginAction = () => {
+    console.log("Login");
+
+    const idValue = idRef.current.value;
+    const pwValue = pwRef.current.value;
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <input type="text" placeholder="ID" />
+      <input type="text" placeholder="ID" ref={idRef} />
       <br />
-      <input type="password" placeholder="PW" />
+      <input type="password" placeholder="PW" ref={pwRef} />
       <br />
       <input
         type="button"
@@ -17,7 +28,7 @@ export default function Login() {
           navigate("/ProJoin");
         }}
       />
-      <input type="button" value="Login" />
+      <input type="button" value="Login" onClick={loginAction} />
     </div>
   );
 }
