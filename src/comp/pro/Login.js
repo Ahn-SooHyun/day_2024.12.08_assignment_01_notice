98668b54e97d77cbe3691da6659b3a060d3ef84f
ASH <soxyxs4996@gmail.com>
2024-12-05 10:29:35 +0900

    Feat_2024.12.05_Save_4_10:29 Navigate 추가

--- src/comp/pro/Login.js
+++ src/comp/pro/Login.js
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Login</h1>
+      <input type="text" placeholder="ID" />
+      <br />
+      <input type="password" placeholder="PW" />
+      <br />
+      <input
+        type="button"
+        value="Register"
+        onClick={() => {
+          navigate("/ProJoin");
+        }}
+      />
+      <input type="button" value="Login" />
     </div>
   );
 }
